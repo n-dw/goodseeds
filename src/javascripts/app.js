d719e29d1269e7d8fee3b628995550fb46566e32
NdeWs <nate@natedewaard.com>
2017-06-15 16:49:26 -0400

    modal stuff

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,1 +1,14 @@
-//import './modules'
+new Vue({
+    el: '#header',
+    delimiters: ['${', '}'],
+    data: {
+        menuOpen: false,
+        navMenuStatus: "mobile-nav--closed"
+    },
+    methods: {
+        toggle: function() {
+            this.menuOpen = !this.menuOpen;
+            this.menuOpen ? this.navMenuStatus = "mobile-nav--open" : this.navMenuStatus = "mobile-nav--closed";
+        },
+    },
+});
