0699d958df62421b70ecda335db3717208c7c5b5
NdeWs <nate@natedewaard.com>
2017-06-19 22:06:12 -0400

    faq

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,6 +1,48 @@
+
+Vue.component('faq', {
+    template: `<div :class="{'faq--active' : isActive}" class="faq" :id="faqId">
+            <h3 @click="activate" class="faq__question">
+                <i :class="icon" ></i>
+                <slot name="question"></slot>
+            </h3>
+            <p class="faq__answer"><slot name="answer"></slot></p>
+        </div>`,
+    props:{
+        open:{
+            type: Boolean,
+            default: false
+        },
+        fid: {default: "faq_un"},
+        openIcon: {default:'icon-plus-1'},
+        closeIcon: {default: 'icon-minus'}
+    },
+    data() {
+        return {
+            isActive: false,
+            icon: 'icon-plus-1',
+            faqId : 'null'
+        };
+    },
+    mounted(){
+        this.isActive = this.open;
+        this.faqId = this.fid;
+        if(this.isActive){
+            this.icon = 'icon-minus';
+        }
+    },
+    methods: {
+        activate: function() {
+            this.isActive = !this.isActive;
+            (this.isActive) ? this.icon = 'icon-minus' : this.icon = 'icon-plus-1';
+        },
+    },
+
+})
+
+
 new Vue({
     el: '#header',
-    delimiters: ['${', '}'],
+    delimiters: ['{|', '|}'],
     data: {
         menuOpen: false,
         navMenuStatus: "mobile-nav--closed"
@@ -11,4 +53,9 @@ new Vue({
             this.menuOpen ? this.navMenuStatus = "mobile-nav--open" : this.navMenuStatus = "mobile-nav--closed";
         },
     },
+});
+
+new Vue({
+    el: '#faqs',
+    delimiters: ['${', '}'],
 });
