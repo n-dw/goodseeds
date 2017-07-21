5c459d7aacd881d746c704d042b569bf7930abdd
NdeWs <nate@natedewaard.com>
2017-07-20 21:21:24 -0400

    babelrc for uglify

--- config/task-config.js
+++ config/task-config.js
@@ -1,39 +1,44 @@
 module.exports = {
-  images      : true,
-  fonts       : true,
-  svgSprite   : true,
-  stylesheets : true,
+    images: true,
+    fonts: true,
+    svgSprite: true,
+    stylesheets: true,
+    ghPages: false,
+    html: false,
+    static: false,
 
-  javascripts: {
-    entry: {
-      // files paths are relative to
-      // javascripts.dest in path-config.json
-      app: ["./app.js"]
+    javascripts: {
+        babel: {
+            presets: [
+                ['es2015', {'modules': false}],
+                'stage-2'
+            ]
+        },
+        entry: {
+            // files paths are relative to
+            // javascripts.dest in path-config.json
+            app: ["./app.js"]
+        },
+        loaders: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+        },
+        ],
+        // This tells webpack middleware where to
+        // serve js files from in development:
+        publicPath: "/assets/js"
     },
-      loaders: [{
-          test: /\.vue$/,
-          loader: 'vue-loader',
-      }],
-      babel: {
-          presets: ['vue']
-      },
-    // This tells webpack middleware where to
-    // serve js files from in development:
-    publicPath: "/assets/js"
-  },
 
-  browserSync: {
-    // Update this to match your development URL
-    proxy: 'queef.dev',
-    files: ['craft/templates/**/*']
-  },
+    browserSync: {
+        // Update this to match your development URL
+        proxy: 'queef.dev',
+        files: ['craft/templates/**/*']
+    },
 
-  production: {
-    rev: true
-  },
+    production: {
+        rev: true
+    },
 
-  ghPages     : false,
-  html        : false,
-  static      : false
+
 }
 
